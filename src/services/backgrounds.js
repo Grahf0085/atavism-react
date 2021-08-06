2ccad22ee881e321e280933c56d9fe98bafb7ba9
Tucker <tuckerhoog@tutanota.com>
2021-08-05 23:03:55 -0400

    backgrounds

--- src/services/backgrounds.js
+++ src/services/backgrounds.js
@@ -4,8 +4,11 @@ import bg3 from '../../public/backgrounds/bg3.jpg';
 import bg4 from '../../public/backgrounds/bg4.jpg';
 import bg5 from '../../public/backgrounds/bg5.jpg';
 import bg6 from '../../public/backgrounds/bg6.jpg';
+import bg7 from '../../public/backgrounds/bg7.jpg';
+import bg8 from '../../public/backgrounds/bg8.jpeg';
+import bg9 from '../../public/backgrounds/bg9.jpg';
 import bg10 from '../../public/backgrounds/bg10.jpg';
 
 export default [
-  bg1, bg2, bg3, bg4, bg5, bg6, bg10
+  bg1, bg2, bg3, bg4, bg5, bg6, bg7, bg8, bg9, bg10
 ];
