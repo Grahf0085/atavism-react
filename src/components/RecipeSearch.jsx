14aadff44081b7898ce1960e8292982a0e6c1001
Tucker <tuckerhoog@tutanota.com>
2021-09-20 15:46:01 -0400

    change from radios to buttons to change what to search for recipes

--- src/components/RecipeSearch.jsx
+++ src/components/RecipeSearch.jsx
@@ -2,25 +2,16 @@ import React, { useState } from 'react';
 import ResultSearch from './ResultSearch';
 import IngredientSearch from './IngredientSearch';
 import styles from '../styles/styles.css';
+import recipeStyles from '../styles/recipes.css';
 
 const RecipeSearch = () => {
 
   const [area, setArea] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  };
-
   return (
     <section className={styles.recipesList}>
-      <form onSubmit={handleSubmit}>
-        <label> Search by Result: 
-          <input type="radio" name="searchBy" value={area} onChange={() => setArea('result')}></input>
-        </label>
-        <label> Search by Ingredient: 
-          <input type="radio" name="searchBy" value={area} onChange={() => setArea('ingredient')}></input>
-        </label>
-      </form>
+      <button className={recipeStyles.button} onClick={() => setArea('result')}>Search by Result</button>
+      <button className={recipeStyles.button} onClick={() => setArea('ingredient')}>Search by Ingredient</button>
       {area === 'result' ? <ResultSearch /> : <section></section> }
       {area === 'ingredient' ? <IngredientSearch /> : <section></section> }
     </section>
